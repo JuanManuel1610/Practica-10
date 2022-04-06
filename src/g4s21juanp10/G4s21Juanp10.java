bc0cd81b0a8b6c962857474ef8d69c77dfc9a133
Juan <>
2022-04-06 12:15:52 -0500

    PRACTICA 10

---
+++ src/g4s21juanp10/G4s21Juanp10.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package g4s21juanp10;
+
+/**
+ *
+ * @author user
+ */
+public class G4s21Juanp10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new principal().setVisible(true);
+        
+    }
+    
+}
